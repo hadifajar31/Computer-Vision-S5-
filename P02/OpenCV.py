f0ad1b7cc1bba3c1c035fd211f9638ffaeacda1a
hadifajar31 <126844385+hadifajar31@users.noreply.github.com>
2023-09-25 19:17:16 +0700

    Create OpenCV.py

---
+++ P02/OpenCV.py
@@ -0,0 +1,22 @@
+# Open Source Computer Vision
+
+# import library open cv
+import cv2
+
+# load image (simpan image dalam 1 folder dengan source sode)
+img = cv2.imread('template.png', 0)
+# tampilkan dalam 1 windows utama
+cv2.imshow('gambar saya 1', img)
+# tunggu action dari user
+cv2.waitKey(0)
+# hapus semua windows(form) yang ada
+cv2.destroyAllWindows()
+
+# load image (simpan image dalam 1 folder dengan source sode)
+img = cv2.imread('template.png', 1)
+# tampilkan dalam 1 windows utama
+cv2.imshow('gambar saya 2', img)
+# tunggu action dari user
+cv2.waitKey(0)
+# hapus semua windows(form) yang ada
+cv2.destroyAllWindows()
