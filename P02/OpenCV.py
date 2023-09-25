636b452bac2b0ad9ffdcd537d772b39cbdd476de
hadifajar31 <126844385+hadifajar31@users.noreply.github.com>
2023-09-25 19:24:47 +0700

    P02

--- P02/OpenCV.py
+++ P02/OpenCV.py
@@ -4,7 +4,7 @@
 import cv2
 
 # load image (simpan image dalam 1 folder dengan source sode)
-img = cv2.imread('template.png', 0)
+img = cv2.imread('template.png', 0) # kode 0 GrayScale
 # tampilkan dalam 1 windows utama
 cv2.imshow('gambar saya 1', img)
 # tunggu action dari user
@@ -13,7 +13,7 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # load image (simpan image dalam 1 folder dengan source sode)
-img = cv2.imread('template.png', 1)
+img = cv2.imread('template.png', 1) # Kode 1 Color
 # tampilkan dalam 1 windows utama
 cv2.imshow('gambar saya 2', img)
 # tunggu action dari user
